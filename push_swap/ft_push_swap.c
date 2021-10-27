bac9fc48d02e94ee0d1d190061ad81b39a9ddc28
sde-alva <sde-alva@student.42sp.org.br>
2021-10-27 16:54:52 -0300

    a little bit more

--- push_swap/ft_push_swap.c
+++ push_swap/ft_push_swap.c
@@ -6,22 +6,36 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 20:32:07 by sde-alva          #+#    #+#             */
-/*   Updated: 2021/10/27 15:37:39 by sde-alva         ###   ########.fr       */
+/*   Updated: 2021/10/27 16:54:45 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
 
 static int	*ft_get_distances(t_stack *stack);
+static int	ft_check_sort_stack_a(t_stack *stk_a);
 
-int	ft_push_swap(t_stack *stack_a, t_stack *stack_b)
+int	ft_push_swap(t_stack *stk_a, t_stack *stk_b)
 {
 	int i;
 	int	max_idx;
 	int	*distances;
 
 	i = 0;
-	distances = ft_get_distances(stack_a);
+	while (!ft_check_sort_stack_a(stk_a))
+	{
+		distances = ft_get_distances(stk_a);
+		if (stk_a->values[stk_a->top] == stk_a->values[stk_a->top - 1] - 1)
+			ft_swap_one(stk_a, 'a');
+		else if (distances[stk_a->top] == 0 && stk_a->values[stk_a->top] < stk_a->size / 2)
+			ft_push(stk_a, stk_b, 'b');
+		else if (distances[stk_a->top] == 1)
+			ft_rotate(stk_a, 'a');
+		else if (distances[stk_a->top] == -1)
+			ft_reverse_rotate(stk_a, 'a');
+		else if (stk_a->values[stk_a->top] < stk_a->size / 2)
+			ft_push(stk_a, stk_b, 'b');
+	}
 
 	return (0);
 }
@@ -43,18 +57,18 @@ static int	*ft_get_distances(t_stack *stack)
 	return (distances);
 }
 
-static int	ft_check_sort_a(t_stack *stack_a)
+static int	ft_check_sort_stack_a(t_stack *stk_a)
 {
 	int	i;
 	int	is_sorted;
 
 	i = 0;
 	is_sorted = 1;
-	if (stack_a->size > stack_a->top + 1)
+	if (stk_a->size > stk_a->top + 1)
 		is_sorted = 0;
-	while (is_sorted && i < stack_a->size - 1)
+	while (is_sorted && i < stk_a->size - 1)
 	{
-		if (stack_a->values[i] < stack_a->values[i + 1])
+		if (stk_a->values[i] < stk_a->values[i + 1])
 		is_sorted = 0;
 		i++;
 	}
